aed4de92b4f518154a2e65421c5433611f65b96d
artvandelay440 <artvandelay440@users.noreply.github.com>
2018-08-20 15:12:49 -0500

    Update 2-SetupDHCP.ps1
    fixed DHCP script not being complete

--- 2-SetupDHCP.ps1
+++ 2-SetupDHCP.ps1
@@ -1,3 +1,32 @@
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
+
+Import-Module DhcpServer
+
+Add-DhcpServerInDC -DnsName 'dc1.testdomain.local' -IPAddress 192.168.202.3
+
+$DHCPScope = @{
+    Name        = 'TestNetwork'
+    StartRange  = '192.168.202.1'
+    EndRange    = '192.168.202.254'
+    SubnetMask  = '255.255.255.0'
+    Description = 'Network for TestDomain'
+    LeaseDuration = (New-TimeSpan -Hours 2)
+    ComputerName = 'dc1.testdomain.local'
+    Type = 'Both'
+    State = 'Active'   
+}
+
+Add-DHCPServerv4Scope @DHCPScope
+
+$ExclusionParams = @{
+    DCDnsName = 'dc1.testdomain.local'
+    ComputerName = 'DC1'
+    StartRange = '192.168.202.1'
+    EndRange = '192.168.202.40'
+    ScopeID = '192.168.202.0'
+}
+
+
 Add-Dhcpserverv4ExclusionRange @ExclusionParams
 
 $ServerV4Scope = @{
